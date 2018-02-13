fded98c426e4693a77713e4a910bb80fb64f1d7b
skckompella <skckompella@gmail.com>
2018-02-13 14:40:51 -0500

    Ordering sentences by count

--- src/ngram_preprocessing.py
+++ src/ngram_preprocessing.py
@@ -11,7 +11,8 @@ def get_agent_msgs(data):
     :param data: Raw conversations
     :return: List of sentences (split by '.' and '?')
     """
-    sentences = set()
+    sentence_count = Counter()
+    sentence_list = list()
     for entry in data['Issues']:
         for m in entry['Messages']:
             if not m['IsFromCustomer']:
@@ -19,15 +20,17 @@ def get_agent_msgs(data):
                 temp = temp.lower()
                 sents = temp.split(".")
                 for s in sents:
-                    sentences.add(constants.START_TOKEN + ' ' + s + ' ' + constants.END_TOKEN)
+                    sentence_count[(constants.START_TOKEN + ' ' + s.strip() + ' ' + constants.END_TOKEN)] += 1
+                    sentence_list.append(constants.START_TOKEN + ' ' + s.strip() + ' ' + constants.END_TOKEN)
                     #Append Start and end token to all sentences
-    return sentences
+    return sentence_list, sentence_count
+
 
 
 def build_inverted_index(sentences):
     """
     Builds an index of word to sentences
-    :param sentences: List of sentences
+    :param sentences: List of unique sentences
     :return: Index of word to sentences
     """
     index = defaultdict(list)
@@ -40,8 +43,8 @@ def build_inverted_index(sentences):
 def build_firstword_index(sentences):
     """
     Build index of first word of all sentences (excluding the start token)
-    :param sentences:
-    :return:
+    :param sentences: List of unique sentences
+    :return: Index of first word to sentences
     """
     index = defaultdict(list)
     for i in range(len(sentences)):
@@ -65,23 +68,30 @@ def build_vocab(sentences):
 
 def main():
     data = utils.read_data_json(constants.RAW_DATA_FILE)
-    sentences = get_agent_msgs(data)
-    # train_sentences = sentences[:-500]
-    # test_sentences = sentences[500:]
-    inverted_index = build_inverted_index(list(sentences))
-    firstword_index = build_firstword_index(list(sentences))
-    vocab = build_vocab(list(sentences))
-    paragraph = ''
-    for s in sentences:
+    sentence_list, sentence_counts = get_agent_msgs(data)
+
+    ordered_unique_sents = []
+    for s in sentence_counts.most_common():
+        ordered_unique_sents.append(s[0])
+    train_sentences = ordered_unique_sents[:-500]
+    test_sentences = ordered_unique_sents[500:]
+    for i in ordered_unique_sents:
+        if "hey" in i:
+            print(i)
+    inverted_index = build_inverted_index(ordered_unique_sents)
+    print(inverted_index['hey'])
+
+    firstword_index = build_firstword_index(ordered_unique_sents)
+    vocab = build_vocab(ordered_unique_sents)
+    paragraph = ""
+    for s in sentence_list:
         paragraph = paragraph + s + ' '
 
     utils.write_data_pkl(firstword_index, constants.FIRSTWORD_INDEX_FILE)
     utils.write_data_pkl(inverted_index, constants.INVERTED_INDEX_FILE)
     utils.write_data_pkl(vocab, constants.VOCAB_FILE)
     utils.write_data_pkl(paragraph, constants.PARAGRAPH_FILE)
-    utils.write_data_pkl(list(sentences), constants.SENTENCES_LIST_FILE)
-
-    # print data['Issues'][5]['Messages'][0]
+    utils.write_data_pkl(ordered_unique_sents, constants.UNIQUE_SENTENCE_LIST_FILE)
 
 
 if __name__ == '__main__':
