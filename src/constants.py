07d3e0ff5b7ff74c3176f9a44847d813d90e8dbc
skckompella <skckompella@gmail.com>
2018-02-13 10:19:33 -0500

    Updating

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+
+RAW_DATA_FILE = "../data/sample_conversations.json"
+INVERTED_INDEX_FILE = "../data/inverted_index.pkl"
+FIRSTWORD_INDEX_FILE = "../data/firstword_index.pkl"
+VOCAB_FILE = "../data/vocab.pkl"
+SENTENCES_LIST_FILE = "../data/sentences.pkl"
+PARAGRAPH_FILE = "../data/paragraph.pkl"
+WORD_MODEL_FILE = "../data/word_model.pkl"
+BIGRAM_MODEL_FILE = "../data/bigram_model.pkl"
+
+
+
+START_TOKEN = '__start__'
+END_TOKEN = '__end__'
+UNK_TOKEN = '__unk__'
